c286090b679af60aa2dc375562869649c864903d
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-04-29 21:41:35 -0300

    add proj

---
+++ src/main/java/com/estudos/learnspringbooks/LearnSpringBooksApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.learnspringbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBooksApplication.class, args);
+	}
+
+}
